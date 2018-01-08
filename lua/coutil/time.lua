2ca259b013171f0538654503c6f7b6e024ea45a5
Renato Maia <maia.renato@gmail.com>
2018-01-08 01:06:10 -0200

    Splitting 'coutil.time' module: API ('coutil.time') and events ('coutil.time.event').

--- lua/coutil/time.lua
+++ lua/coutil/time.lua
@@ -2,71 +2,15 @@ local _G = require "_G"                                                         
 local assert = _G.assert
 local type = _G.type
 
-local array = require "table"
-local insert = array.insert
-local remove = array.remove
-
-local math = require "math"
-local floor = math.floor
-local inf = math.huge
-
 local os = require "os"
 local gettime = os.time
 
 local event = require "coutil.event"
 local awaitevent = event.await
-local emitevent = event.emit
-local pendingevent = event.pending
 
-local function sortedinsert(list, value, i, j)
-	if i > j then
-		return insert(list, i, value)
-	else
-		local m = floor((i+j)/2)
-		local pivot = list[m]
-		if value < pivot then
-			return sortedinsert(list, value, i, m-1)
-		elseif value > pivot then
-			return sortedinsert(list, value, m+1, j)
-		end
-	end
-end
-
-local function sortedremove(list, value, i, j)
-	if i <= j then
-		local m = floor((i+j)/2)
-		local pivot = list[m]
-		if value < pivot then
-			return sortedremove(list, value, i, m-1)
-		elseif value > pivot then
-			return sortedremove(list, value, m+1, j)
-		else
-			return remove(list, m)
-		end
-	end
-end
-
-local waketimes = {}
-
-local function emituntil(timestamp)                                             --[[VERBOSE]] verbose:time(true, "emitting timers before ",timestamp)
-	while waketimes[1] ~= nil and waketimes[1] <= timestamp do                    --[[VERBOSE]] verbose:time("emit timer ",waketimes[1])
-		emitevent(remove(waketimes, 1))
-	end                                                                           --[[VERBOSE]] verbose:time(false, "timers emitted")
-end
-
-local function setuptimer(timestamp)
-	assert(type(timestamp) == "number", "bad argument #1 (number expected)")
-	if timestamp ~= inf then                                                      --[[VERBOSE]] verbose:time("setup timer ",timestamp)
-		sortedinsert(waketimes, timestamp, 1, #waketimes)
-	end
-end
-
-local function canceltimer(timestamp)
-	assert(type(timestamp) == "number", "bad argument #1 (number expected)")
-	if not pendingevent(timestamp) then                                           --[[VERBOSE]] verbose:time("clear timer ",timestamp)
-		sortedremove(waketimes, timestamp, 1, #waketimes)
-	end
-end
+local timevt = require "coutil.time.event"
+local setuptimer = timevt.create
+local emituntil = timevt.emitall
 
 local function waituntil(timestamp)
 	setuptimer(timestamp)
@@ -74,10 +18,8 @@ local function waituntil(timestamp)
 end
 
 local module = {
-	setuptimer = setuptimer,
-	canceltimer = canceltimer,
-	waituntil = waituntil,
 	gettime = gettime,
+	waituntil = waituntil,
 }
 
 function module.setclock(func)
@@ -92,7 +34,7 @@ end
 
 function module.run(idle, timeout)
 	while true do
-		local nextwake = waketimes[1]                                               --[[VERBOSE]] verbose:time("next wake is ",nextwake)
+		local nextwake = emituntil(gettime())                                       --[[VERBOSE]] verbose:time("next wake is ",nextwake)
 		if nextwake == nil then
 			return
 		end
@@ -103,7 +45,6 @@ function module.run(idle, timeout)
 		if nextwake > now then                                                      --[[VERBOSE]] verbose:time(true, "wait until next wake")
 			idle(nextwake)                                                            --[[VERBOSE]] verbose:time(false, "wait done")
 		end
-		emituntil(gettime())
 	end
 end
 
