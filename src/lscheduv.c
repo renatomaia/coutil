1a7729e5cf77d2b07f2054713ab48809bd718248
Renato Maia <maia.renato@gmail.com>
2019-04-09 05:45:44 -0300

    Add support to signal in module 'scheduler'.

--- src/lscheduv.c
+++ src/lscheduv.c
@@ -30,6 +30,8 @@ LUALIB_API void luaL_printstack(lua_State *L)
 	printf("\n");
 }
 
+#include <signal.h>
+#include <string.h>
 #include <uv.h>
 
 
@@ -74,6 +76,13 @@ static void cosL_checkerr (lua_State *L, int err) {
 	if (err < 0) cosL_error(L, err);
 }
 
+static int cosL_yieldk (lua_State *L, int narg, lua_KContext ctx,
+                                                lua_KFunction func,
+                                                uv_handle_t *h) {
+	h->data = L;  /* mark as rescheduled */
+	return lua_yieldk(L, narg, ctx, func);
+}
+
 
 /* Function 'run' (uv_loop_t) */
 
@@ -114,17 +123,17 @@ static void cosL_resumehandle (uv_handle_t *h, lua_State *co) {
 	     !uv_is_closing(h) ) uv_close(h, cosB_onclosed);
 }
 
-static void cosL_resethandlek (lua_State *L, uv_handle_type t, int narg,
-                               lua_KContext ctx, lua_KFunction func) {
+static uv_handle_t *cosL_resethandlek (lua_State *L, uv_handle_type t, int narg,
+                                       lua_KContext ctx, lua_KFunction func) {
 	uv_handle_t *h = cosL_tohandle(L);
 	if (!lua_isyieldable(L)) luaL_error(L, "unable to yield");
-	h->data = L;  /* mark as rescheduled */
 	if (h->type == UV_UNKNOWN_HANDLE) {
-		func(L, LUA_YIELD, ctx);
+		func(L, LUA_YIELD, ctx);  /* never returns */
 	} else if ( h->type != t || uv_is_closing(h) ) {
 		if (!uv_is_closing(h)) uv_close(h, cosB_onclosed);
-		lua_yieldk(L, narg, ctx, func);
+		cosL_yieldk(L, narg, ctx, func, h);  /* never returns */
 	}
+	return h;
 }
 
 static int cosK_checkcancelled (lua_State *L, int status, lua_KContext ctx) {
@@ -151,25 +160,123 @@ static void cosB_onidle (uv_idle_t *h) {
 static int cosK_setasidle (lua_State *L, int status, lua_KContext ctx) {
 	uv_loop_t *loop = cosL_toloop(L);
 	uv_idle_t *h = (uv_idle_t *)cosL_tohandle(L);
-	int err = uv_idle_init(loop, h);
+	int err;
+	cosL_checkerr(L, uv_idle_init(loop, h));
 	cosL_refcoro((uv_handle_t *)h, L);
-	cos_assert(status == LUA_YIELD);
-	cos_assert(!ctx);
-	if (err >= 0) {
-		err = uv_idle_start(h, cosB_onidle);
-		if (err >= 0) {
-			h->data = L;  /* mark as rescheduled */
-			return lua_yieldk(L, lua_gettop(L), 0, cosK_checkcancelled);
-		}
+	err = uv_idle_start(h, cosB_onidle);
+	if (err < 0) {
 		uv_close((uv_handle_t *)h, cosB_onclosed);
+		return cosL_error(L, err);
 	}
-	return cosL_error(L, err);
+	return cosL_yieldk(L, lua_gettop(L), 0, cosK_checkcancelled,
+	                   (uv_handle_t *)h);
 }
 
 static int cosM_pause (lua_State *L) {
 	int narg = lua_gettop(L);
-	cosL_resethandlek(L, UV_IDLE, narg, 0, cosK_setasidle);
-	return lua_yieldk(L, narg, 0, cosK_checkcancelled);
+	uv_handle_t *h = cosL_resethandlek(L, UV_IDLE, narg, 0, cosK_setasidle);
+	return cosL_yieldk(L, narg, 0, cosK_checkcancelled, h);
+}
+
+
+/* Function 'signal' (uv_signal_t) */
+
+static const struct { const char * const name; const int value; } signals[] = {
+	{ "abort", SIGABRT },
+	{ "continue", SIGCONT },
+	{ "hangup", SIGHUP },
+	{ "interrupt", SIGINT },
+	{ "quit", SIGQUIT },
+	{ "stop", SIGTSTP },
+	{ "terminate", SIGTERM },
+	{ "badpipe", SIGPIPE },
+	{ "bgread", SIGTTIN },
+	{ "bgwrite", SIGTTOU },
+	{ "cpulimit", SIGXCPU },
+	{ "filelimit", SIGXFSZ },
+	{ "child", SIGCHLD },
+	{ "clocktime", SIGALRM },
+	{ "debug", SIGTRAP },
+	{ "urgsock", SIGURG },
+	{ "user1", SIGUSR1 },
+	{ "user2", SIGUSR2 },
+#ifdef SIGPROF
+	{ "cputimall", SIGPROF },
+#endif
+#ifdef SIGVTALRM
+	{ "cputimprc", SIGVTALRM },
+#endif
+#ifdef SIGPOLL
+	{ "polling", SIGPOLL },
+#endif
+#ifdef SIGSYS
+	{ "syscall", SIGSYS },
+#endif
+#ifdef SIGWINCH
+	{ "winresize", SIGWINCH },
+#endif
+	{ NULL, 0 },
+};
+
+static void cosL_pushsignal (lua_State *L, int signum) {
+	int i;
+	for (i=0; signals[i].name; i++) {
+		if (signals[i].value == signum) {
+			lua_pushstring(L, signals[i].name);
+			return;
+		}
+	}
+	lua_pushnil(L);
+}
+
+static int cosL_checksignal (lua_State *L, int arg, const char *def) {
+	const char *name = (def) ? luaL_optstring(L, arg, def) :
+	                           luaL_checkstring(L, arg);
+	int i;
+	for (i=0; signals[i].name; i++)
+		if (strcmp(signals[i].name, name) == 0)
+			return signals[i].value;
+	return luaL_argerror(L, arg,
+	                     lua_pushfstring(L, "invalid signal '%s'", name));
+}
+
+static void cosB_onsignal (uv_signal_t *h, int signum) {
+	lua_State *co = (lua_State *)h->data;
+	cos_assert(co != NULL);
+	lua_settop(co, 0);
+	cosL_pushsignal(co, signum);
+	cosL_resumehandle((uv_handle_t *)h, co);
+}
+
+static void cosL_startsignal (lua_State *L, uv_signal_t *h, int signal) {
+	int err = uv_signal_start(h, cosB_onsignal, signal);
+	if (err < 0) {
+		uv_close((uv_handle_t *)h, cosB_onclosed);
+		cosL_error(L, err);  /* never returns */
+	}
+}
+
+static int cosK_setonsignal (lua_State *L, int status, lua_KContext ctx) {
+	uv_loop_t *loop = cosL_toloop(L);
+	uv_signal_t *h = (uv_signal_t *)cosL_tohandle(L);
+	int signal = (int)ctx;
+	cosL_checkerr(L, uv_signal_init(loop, h));
+	cosL_refcoro((uv_handle_t *)h, L);
+	cosL_startsignal(L, h, signal);
+	return cosL_yieldk(L, lua_gettop(L)-1, 1, cosK_checkcancelled, 
+	                   (uv_handle_t *)h);
+}
+
+static int cosM_awaitsig (lua_State *L) {
+	int signal = cosL_checksignal(L, 1, NULL);
+	int narg = lua_gettop(L)-1;
+	uv_signal_t *h = (uv_signal_t *)cosL_resethandlek(L, UV_SIGNAL, narg,
+		(lua_KContext)signal, cosK_setonsignal);
+	if (h->signum != signal) {
+		cosL_checkerr(L, uv_signal_stop(h));
+		cosL_startsignal(L, h, signal);
+	}
+	return cosL_yieldk(L, narg, 1, cosK_checkcancelled, (uv_handle_t *)h);
 }
 
 
@@ -195,6 +302,7 @@ LUAMOD_API int luaopen_coutil_scheduler (lua_State *L)
 	static const luaL_Reg modf[] = {
 		{"run", cosM_run},
 		{"pause", cosM_pause},
+		{"awaitsig", cosM_awaitsig},
 		{NULL, NULL}
 	};
 	luaL_newlibtable(L, modf);
