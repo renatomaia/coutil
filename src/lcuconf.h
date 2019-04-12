4fa0c13d488376ba4371001f6cc391747b92587b
Renato Maia <maia.renato@gmail.com>
2019-04-12 06:50:52 -0300

    Organize 'coutil.scheduler' code into many files.
    - Adopt the new exported declaration prefix 'lcu_'.
    - Organization for modular implementation of new features.

---
+++ src/lcuconf.h
@@ -0,0 +1,14 @@
+#ifndef lcuconf_h
+#define lcuconf_h
+
+
+#ifndef LCULIB_API
+#define LCULIB_API
+#endif
+
+#if !defined(lcu_assert)
+#define lcu_assert(X)	((void)(X))
+#endif
+
+
+#endif
---
+++ src/lschemod.c
@@ -0,0 +1,20 @@
+#include "lmodaux.h"
+
+LCULIB_API void lcuM_addscheduf (lua_State *L);
+
+LCULIB_API void lcuM_addtimef (lua_State *L);
+
+LCULIB_API void lcuM_addsignalf (lua_State *L);
+
+
+LUAMOD_API int luaopen_coutil_scheduler (lua_State *L)
+{
+	lcuM_newmodupvs(L, NULL);
+	lua_newtable(L);
+	lcuM_addscheduf(L);
+	lcuM_addtimef(L);
+	lcuM_addsignalf(L);
+	lua_insert(L, -(LCU_MODUPVS+1));
+	lua_pop(L, LCU_MODUPVS);
+	return 1;
+}
