492fe52c91cc2477458d93eecab3eb89729813f6
Renato Maia <maia.renato@gmail.com>
2019-04-13 06:58:39 -0300

    Rename module 'coutil.scheduler' to 'coutil.system'.

--- src/lsysmod.c
+++ src/lsysmod.c
@@ -7,7 +7,7 @@ LCULIB_API void lcuM_addtimef (lua_State *L);
 LCULIB_API void lcuM_addsignalf (lua_State *L);
 
 
-LUAMOD_API int luaopen_coutil_scheduler (lua_State *L)
+LUAMOD_API int luaopen_coutil_system (lua_State *L)
 {
 	lcuM_newmodupvs(L, NULL);
 	lua_newtable(L);
