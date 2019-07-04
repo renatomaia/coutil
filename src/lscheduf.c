b92f32b4c584a6c720eb4b4df50c452ed14512d5
Renato Maia <maia.renato@gmail.com>
2019-07-03 23:04:17 -0300

    feat: add support to stop processing system events
    Add function 'halt' in module 'coutil.system' to make function
    'system.run' return prematurely and stop processing system events used
    to resume coroutines.
    
    Release-Note: Support to stop processing system events.

--- src/lscheduf.c
+++ src/lscheduf.c
@@ -6,13 +6,20 @@ static int lcuM_run (lua_State *L) {
 	uv_loop_t *loop = lcu_toloop(L);
 	int pending;
 	int mode = luaL_checkoption(L, 1, "loop", opts);
-	luaL_argcheck(L, !loop->data, 1, "already running");
+	if (loop->data) luaL_error(L, "already running");
 	lua_settop(L, 2);  /* set trap function as top */
 	loop->data = (void *)L;
 	pending = uv_run(loop, mode);
 	loop->data = NULL;
 	lua_pushboolean(L, pending);
 	return 1;
+}
+
+static int lcuM_halt (lua_State *L) {
+	uv_loop_t *loop = lcu_toloop(L);
+	if (!loop->data) luaL_error(L, "not running");
+	uv_stop(loop);
+	return 0;
 }
 
 static int lcuM_printall (lua_State *L) {
@@ -24,6 +31,7 @@ static int lcuM_printall (lua_State *L) {
 LCULIB_API void lcuM_addscheduf (lua_State *L) {
 	static const luaL_Reg modf[] = {
 		{"run", lcuM_run},
+		{"halt", lcuM_halt},
 		{"printall", lcuM_printall},
 		{NULL, NULL}
 	};
