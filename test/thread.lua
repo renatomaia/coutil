33298c79aba55320957b637d6f0de4371ea2ad74
Renato Maia <maia.renato@gmail.com>
2020-04-23 22:27:48 -0300

    fixup! Add preemptive thread support (DRAFT).

--- test/thread.lua
+++ test/thread.lua
@@ -7,9 +7,16 @@ file:write[[
 	local debug = require "debug"
 	local io = require "io"
 	local os = require "os"
-	local path = ...
+	local path, yield = ...
 	local ok, err = xpcall(function ()
+		if yield then
+			local coroutine = require "coroutine"
+			yield = coroutine.yield
+		else
+			yield = function () end
+		end
 		repeat
+			assert(select("#", yield(1, 2, 3)) == 0)
 			local contents
 			local file = io.open(path)
 			if file then
@@ -21,6 +28,7 @@ file:write[[
 	end, debug.traceback)
 	if not ok then
 		io.stderr:write(err)
+		io.stderr:flush()
 	end
 ]]
 file:close()
@@ -36,273 +44,389 @@ local function sendignal(path)
 	end
 end
 
-local function waituntil(cond)
-	cond()
-end
-
 newtest "threads" --------------------------------------------------------------
 
-assert(system.threads() == nil)
-
-local t = assert(system.threads(1))
-assert(t:getcount("size") == 1)
-assert(t:getcount("thread") == 0)
-assert(t:getcount("tasks") == 0)
-assert(t:getcount("running") == 0)
-assert(t:getcount("pending") == 0)
-
-local path1 = os.tmpname()
-assert(t:dofile(waitsignal, "t", path1) == true)
-assert(t:getcount("size") == 1)
-assert(t:getcount("thread") == 1)
-assert(t:getcount("tasks") == 1)
-repeat until (t:getcount("running") == 1)
-assert(t:getcount("pending") == 0)
-
-local path2 = os.tmpname()
-assert(t:dofile(waitsignal, "t", path2) == true)
-assert(t:getcount("size") == 1)
-assert(t:getcount("thread") == 1)
-assert(t:getcount("tasks") == 2)
-assert(t:getcount("running") == 1)
-assert(t:getcount("pending") == 1)
-
-sendignal(path1)
-assert(t:getcount("size") == 1)
-assert(t:getcount("thread") == 1)
-repeat until (t:getcount("tasks") == 1)
-assert(t:getcount("running") == 1)
-assert(t:getcount("pending") == 0)
-
-assert(t:dofile(waitsignal, "t", path1) == true)
-assert(t:getcount("size") == 1)
-assert(t:getcount("thread") == 1)
-repeat until (t:getcount("tasks") == 2)
-assert(t:getcount("running") == 1)
-assert(t:getcount("pending") == 1)
-
-assert(t:resize(4) == true)
-assert(t:getcount("size") == 4)
-assert(t:getcount("thread") == 2)
-assert(t:getcount("tasks") == 2)
-repeat until (t:getcount("running") == 2)
-assert(t:getcount("pending") == 0)
-
-assert(t:resize(1) == true)
-assert(t:getcount("size") == 1)
-assert(t:getcount("thread") == 2)
-assert(t:getcount("tasks") == 2)
-assert(t:getcount("running") == 2)
-assert(t:getcount("pending") == 0)
-
-local path3 = os.tmpname()
-assert(t:dofile(waitsignal, "t", path3) == true)
-assert(t:getcount("size") == 1)
-assert(t:getcount("thread") == 2)
-assert(t:getcount("tasks") == 3)
-assert(t:getcount("running") == 2)
-assert(t:getcount("pending") == 1)
-
-sendignal(path1)
-assert(t:getcount("size") == 1)
-repeat until (t:getcount("thread") == 1)
-assert(t:getcount("tasks") == 2)
-assert(t:getcount("running") == 1)
-assert(t:getcount("pending") == 1)
-
-assert(t:resize(0) == true)
-assert(t:getcount("size") == 0)
-assert(t:getcount("thread") == 1)
-assert(t:getcount("tasks") == 2)
-assert(t:getcount("running") == 1)
-assert(t:getcount("pending") == 1)
-
-sendignal(path2)
-assert(t:getcount("size") == 0)
-repeat until (t:getcount("thread") == 0)
-assert(t:getcount("tasks") == 1)
-assert(t:getcount("running") == 0)
-assert(t:getcount("pending") == 1)
-
-assert(t:close() == true)
-
-do return end
-
-
-local path3 = os.tmpname()
-assert(t:dofile(waitsignal, "t", path3) == true)
-assert(t:getcount("size") == 1)
-assert(t:getcount("thread") == 1)
-assert(t:getcount("tasks") == 2)
-assert(t:getcount("running") == 1)
-assert(t:getcount("pending") == 1)
 
 do case "error messages"
-	asserterr(system.tpool() == nil)
 	for v in ipairs(types) do
 		if type(v) ~= "number" then
-			asserterr("number expected", pcall(system.tpool, v))
+			asserterr("number expected", pcall(system.threads, v))
 		end
 	end
-	asserterr("integer expected", pcall(system.tpool, math.pi))
-	asserterr("invalid size", pcall(system.tpool, -1))
+	asserterr("number has no integer representation",
+		pcall(system.threads, math.pi))
 
 	done()
 end
 
-do case "zero sized"
-	local tpool = assert(system.tpool(0))
-	tpool:dostring("")
-	tpool:dostring("")
-	tpool:dostring("")
+do case "compilation errors"
+	local t = assert(system.threads(0))
 
-	local defined = tpool:getcount("size")
-	local actual = tpool:getcount("thread")
-	local pending = tpool:getcount("pending")
+	for v in ipairs(types) do
+		local ltype = type(v)
+		if ltype ~= "string" and ltype ~= "number" then
+			asserterr("string or memory expected", pcall(t.dostring, t, v))
+			asserterr("string expected", pcall(t.dostring, t, "", v))
+			asserterr("string expected", pcall(t.dostring, t, "", "", v))
+		end
+	end
 
-	os.execute("sleep 1")
-	assert(io.open(path) == nil)
-
-	tpool:resize(1)
-	waitsynced(path)
-
-	done()
-end
-
-do case "zero sized"
-	local tpool = assert(system.tpool(0))
-	local path = startsynced(tpool, "")
-
-	os.execute("sleep 1")
-	assert(io.open(path) == nil)
-
-	tpool:resize(1)
-	waitsynced(path)
+	local bytecodes = string.dump(function () a = a+1 end)
+	asserterr("attempt to load a binary chunk (mode is 't')",
+	          t:dostring(bytecodes, nil, "t"))
+	asserterr("attempt to load a text chunk (mode is 'b')",
+	          t:dostring("a = a+1", "bytecodes", "b"))
+	asserterr("syntax error", t:dostring("invalid chunk"))
 
 	done()
 end
 
-do case "zero sized"
-	asserterr("string or memory expected", pcall(system.thread))
-	asserterr("string or memory expected", pcall(system.thread, function () end))
-	asserterr("string expected", pcall(system.thread, "", function () end))
-	asserterr("string expected", pcall(system.thread, "", "empty", function () end))
-	--asserterr("invalid argument", pcall(system.thread, "", "empty", "t", function () end))
-	--asserterr("invalid argument", pcall(system.thread, "", nil, nil, function () end))
-	asserterr("syntax error", system.thread("invalid chunk"))
+do case "runtime errors"
+	local t = assert(system.threads(1))
+	assert(t:dostring[[
+		require "_G"
+		error "Oops!"
+	]] == true)
+	repeat until (t:count("tasks") == 0)
+	assert(t:close() == true)
 
 	done()
 end
 
-do case "ignore runtime errors"
-	assert(system.thread[[ error "Oops!" ]] == true)
+do case "type errors"
+	local t = assert(system.threads(1))
+	asserterr("bad argument #5 (illegal type)",
+		t:dostring("", nil, "t", table))
+	asserterr("bad argument #6 (illegal type)",
+		t:dostring("", nil, "t", 1, print))
+	asserterr("bad argument #7 (illegal type)",
+		t:dostring("", nil, "t", 1, 2, coroutine.running()))
+	asserterr("bad argument #8 (illegal type)",
+		t:dostring("", nil, "t", 1, 2, 3, io.stdout))
 
-	--done()
+	done()
 end
 
-do return end
+do case "not contained"
+	assert(system.threads() == nil)
 
-do case "parallel execution"
-	local path = os.tmpname()
-	os.remove(path)
+	done()
+end
 
-	assert(io.open(path) == nil)
-	assert(system.thread(string.format([[
-		local io = require "io"
-		local os = require "os"
-		local path = %q
-		while true do
-			local file = io.open(path)
-			if file ~= nil then
-				local contents = file:read("a")
-				file:close()
-				if contents  == "deleteme" then
-					os.remove(path)
-					break
+do case "no threads"
+	local t = assert(system.threads(-1))
+	assert(t:count("size") == 0)
+	assert(t:count("threads") == 0)
+	assert(t:count("tasks") == 0)
+	assert(t:count("running") == 0)
+	assert(t:count("pending") == 0)
+	assert(t:close() == true)
+
+	local t = assert(system.threads(0))
+	assert(t:count("size") == 0)
+	assert(t:count("threads") == 0)
+	assert(t:count("tasks") == 0)
+	assert(t:count("running") == 0)
+	assert(t:count("pending") == 0)
+
+	assert(t:dostring[[repeat until false]] == true)
+	assert(t:count("size") == 0)
+	assert(t:count("threads") == 0)
+	assert(t:count("tasks") == 1)
+	assert(t:count("running") == 0)
+	assert(t:count("pending") == 1)
+
+	assert(t:close() == true)
+
+	done()
+end
+
+do case "no tasks"
+	local t = assert(system.threads(3))
+	assert(t:count("size") == 3)
+	assert(t:count("threads") == 3)
+	assert(t:count("tasks") == 0)
+	assert(t:count("running") == 0)
+	assert(t:count("pending") == 0)
+
+	assert(t:close() == true)
+
+	done()
+end
+
+do case "yielding tasks"
+	local t = assert(system.threads(1))
+	local path = { n = 5 }
+	for i = 1, path.n do
+		path[i] = os.tmpname()
+		assert(t:dofile(waitsignal, "t", path[i], "yield") == true)
+	end
+
+	assert(t:count("size") == 1)
+	assert(t:count("threads") == 1)
+	assert(t:count("tasks") == path.n)
+	repeat until (t:count("running") == 1)
+	assert(t:count("pending") == path.n-1)
+
+	while path.n > 0 do
+		sendignal(path[path.n])
+		path.n = path.n-1
+		assert(t:count("size") == 1)
+		assert(t:count("threads") == 1)
+		repeat until (t:count("tasks") == path.n)
+		if path.n > 0 then
+			assert(t:count("running") == 1)
+			assert(t:count("pending") == path.n-1)
+		else
+			assert(t:count("running") == 0)
+			assert(t:count("pending") == 0)
+		end
+	end
+
+	assert(t:count("size") == 1)
+	assert(t:count("threads") == 1)
+	assert(t:count("tasks") == 0)
+	assert(t:count("running") == 0)
+	assert(t:count("pending") == 0)
+	assert(t:close() == true)
+
+	done()
+end
+
+do case "many threads, even more tasks"
+	local path = {}
+	local m, n = 50, 5
+	local t = assert(system.threads(n))
+	for i = 1, m do
+		path[i] = os.tmpname()
+		assert(t:dostring([[
+			local _G = require "_G"
+			local io = require "io"
+			local debug = require "debug"
+			local coroutine = require "coroutine"
+			local path = ...
+			local ok, err = xpcall(function ()
+				while true do
+					local file = io.open(path)
+					if file == nil then break end
+					file:close()
+					coroutine.yield()
 				end
+				local file = assert(io.open(path, "w"))
+				file:write(path)
+				file:close()
+			end, debug.traceback)
+			if not ok then
+				io.stderr:write(err)
+				io.stderr:flush()
 			end
-		end
-	]], path)) == true)
-
-	local file = assert(io.open(path, "w"))
-	file:write("deleteme")
-	file:close()
-
-	local removed = false
-	for i=1, 1e3 do
-		if io.open(path) == nil then
-			removed = true
-			break
-		end
+		]], nil, "t", path[i]) == true)
 	end
 
-	assert(removed)
+	assert(t:count("size") == n)
+	assert(t:count("threads") == n)
+	assert(t:count("tasks") == m)
+	repeat until (t:count("running") == n)
+	assert(t:count("pending") == m-n)
 
-	done()
-end
+	for _, path in ipairs(path) do
+		os.remove(path)
+	end
 
+	repeat until (t:count("tasks") == 0)
+	assert(t:count("size") == n)
+	assert(t:count("threads") == n)
+	assert(t:count("running") == 0)
+	assert(t:count("pending") == 0)
+	assert(t:close() == true)
 
-local function startsynced(chunk, ...)
-	local path = os.tmpname()
-	os.remove(path)
-
-	assert(io.open(path) == nil)
-	assert(system.thread(string.format([[%s
-		local io = require "io"
-		local os = require "os"
-		local path = %q
-		local file = io.open(path, "w")
-		file:write("OK")
+	for _, path in ipairs(path) do
+		local file = assert(io.open(path))
+		assert(file:read() == path)
 		file:close()
-	]], chunk, path), ...) == true)
-
-	return path
-end
-
-local function waitsynced(path)
-	local result
-	for i=1, 1e3 do
-		result = readfrom(path)
-		if result ~= nil then
-			break
-		end
+		os.remove(path)
 	end
-
-	assert(result == "OK")
-end
-
-do case "arguments"
-	waitsynced(startsynced("assert(... == nil)"))
-	waitsynced(startsynced("assert(... == nil)", nil))
-	waitsynced(startsynced("assert(... == false)", false))
-	waitsynced(startsynced("assert(... == true)", true))
-	waitsynced(startsynced("assert(... == 0)", 0))
-	waitsynced(startsynced("assert(... == 1)", 1))
-	waitsynced(startsynced("assert(... == -1)", -1))
-	waitsynced(startsynced("assert(... == 0xadap-16)", 0xadap-16))
-	waitsynced(startsynced("assert(... == '')", ""))
-	waitsynced(startsynced("assert(... == 'Lua 5.4')", "Lua 5.4"))
-	waitsynced(startsynced("assert(... == '\\0')", "\0"))
-	waitsynced(startsynced([[
-		local a,b,c,d,e = ...
-		assert(a == nil)
-		assert(b == false)
-		assert(c == 123)
-		assert(d == 0xfacep-8)
-		assert(e == "\001")
-	]]), nil, false, 123, 0xfacep-8, "\001")
 
 	done()
 end
 
-do case "yield"
-	local synced = startsynced[[
-		for i = 1, 10 do
-			coroutine.yield()
-		end
-	]]
+do case "pending tasks"
+	local t = assert(system.threads(1))
+	local path1 = os.tmpname()
+	assert(t:dofile(waitsignal, "t", path1) == true)
+	assert(t:count("size") == 1)
+	assert(t:count("threads") == 1)
+	assert(t:count("tasks") == 1)
+	repeat until (t:count("running") == 1)
+	assert(t:count("pending") == 0)
 
-	waitsynced(synced)
+	local path2 = os.tmpname()
+	assert(t:dofile(waitsignal, "t", path2) == true)
+	assert(t:count("size") == 1)
+	assert(t:count("threads") == 1)
+	assert(t:count("tasks") == 2)
+	assert(t:count("running") == 1)
+	assert(t:count("pending") == 1)
+
+	sendignal(path1)
+	assert(t:count("size") == 1)
+	assert(t:count("threads") == 1)
+	repeat until (t:count("tasks") == 1)
+	assert(t:count("running") == 1)
+	assert(t:count("pending") == 0)
+
+	sendignal(path2)
+	assert(t:count("size") == 1)
+	assert(t:count("threads") == 1)
+	repeat until (t:count("tasks") == 0)
+	assert(t:count("running") == 0)
+	assert(t:count("pending") == 0)
+
+	assert(t:close() == true)
+
+	done()
+end
+
+do case "idle threads"
+	local t = assert(system.threads(2))
+	assert(t:resize(4) == true)
+	assert(t:count("size") == 4)
+	assert(t:count("threads") == 2)
+	assert(t:count("tasks") == 0)
+	assert(t:count("running") == 0)
+	assert(t:count("pending") == 0)
+
+	local path3 = os.tmpname()
+	assert(t:dofile(waitsignal, "t", path3) == true)
+	assert(t:count("size") == 4)
+	assert(t:count("threads") == 2)
+	assert(t:count("tasks") == 1)
+	repeat until (t:count("running") == 1)
+	assert(t:count("pending") == 0)
+
+	local path4 = os.tmpname()
+	assert(t:dofile(waitsignal, "t", path4) == true)
+	assert(t:count("size") == 4)
+	assert(t:count("threads") == 2)
+	assert(t:count("tasks") == 2)
+	repeat until (t:count("running") == 2)
+	assert(t:count("pending") == 0)
+
+	sendignal(path3)
+	sendignal(path4)
+	repeat until (t:count("running") == 0)
+	assert(t:count("size") == 4)
+	assert(t:count("threads") == 2)
+	assert(t:count("tasks") == 0)
+
+	assert(t:close() == true)
+
+	done()
+end
+
+do case "increase size"
+	local t = assert(system.threads(1))
+	local path1 = os.tmpname()
+	assert(t:dofile(waitsignal, "t", path1) == true)
+	local path2 = os.tmpname()
+	assert(t:dofile(waitsignal, "t", path2) == true)
+
+	assert(t:count("size") == 1)
+	assert(t:count("threads") == 1)
+	assert(t:count("tasks") == 2)
+	repeat until (t:count("running") == 1)
+	assert(t:count("pending") == 1)
+
+	assert(t:resize(4) == true)
+	assert(t:count("size") == 4)
+	assert(t:count("threads") == 2)
+	assert(t:count("tasks") == 2)
+	repeat until (t:count("running") == 2)
+	assert(t:count("pending") == 0)
+
+	sendignal(path1)
+	sendignal(path2)
+	repeat until (t:count("running") == 0)
+	assert(t:count("size") == 4)
+	assert(t:count("threads") == 2)
+	assert(t:count("tasks") == 0)
+	assert(t:count("pending") == 0)
+
+	assert(t:close() == true)
+
+	done()
+end
+
+do case "decrease size"
+	local t = assert(system.threads(4))
+	local path1 = os.tmpname()
+	assert(t:dofile(waitsignal, "t", path1) == true)
+	local path2 = os.tmpname()
+	assert(t:dofile(waitsignal, "t", path2) == true)
+	repeat until (t:count("running") == 2)
+	assert(t:count("size") == 4)
+	assert(t:count("threads") == 4)
+	assert(t:count("tasks") == 2)
+	assert(t:count("running") == 2)
+	assert(t:count("pending") == 0)
+
+	assert(t:resize(1) == true)
+	assert(t:count("size") == 1)
+	repeat until (t:count("threads") == 2)
+	assert(t:count("tasks") == 2)
+	assert(t:count("running") == 2)
+	assert(t:count("pending") == 0)
+
+	assert(t:dostring[[repeat until false]] == true)
+	assert(t:count("size") == 1)
+	assert(t:count("threads") == 2)
+	assert(t:count("tasks") == 3)
+	assert(t:count("running") == 2)
+	assert(t:count("pending") == 1)
+
+	sendignal(path1)
+	assert(t:count("size") == 1)
+	repeat until (t:count("threads") == 1)
+	assert(t:count("tasks") == 2)
+	assert(t:count("running") == 1)
+	assert(t:count("pending") == 1)
+
+	assert(t:resize(0) == true)
+	assert(t:count("size") == 0)
+	assert(t:count("threads") == 1)
+	assert(t:count("tasks") == 2)
+	assert(t:count("running") == 1)
+	assert(t:count("pending") == 1)
+
+	sendignal(path2)
+	assert(t:count("size") == 0)
+	repeat until (t:count("threads") == 0)
+	assert(t:count("tasks") == 1)
+	assert(t:count("running") == 0)
+	assert(t:count("pending") == 1)
+
+	assert(t:close() == true)
+
+	done()
+end
+
+do case "collect pending"
+	assert(system.threads(0):dostring[[repeat until false]])
+
+	done()
+end
+
+do case "collect running"
+	local path1 = os.tmpname()
+
+	do
+		local t = assert(system.threads(1))
+		assert(t:dofile(waitsignal, "t", path1) == true)
+		repeat until (t:count("running") == 1)
+	end
+
+	gc()
+	sendignal(path1)
 
 	done()
 end
