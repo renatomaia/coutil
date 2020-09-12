ca37801e9dea7d81996bffe3ce94c23386442e62
Renato Maia <maia.renato@gmail.com>
2020-09-12 18:54:08 -0300

    refactor: adjust to Lua 5.4

--- demo/remoteexec/client.lua
+++ demo/remoteexec/client.lua
@@ -1,17 +1,17 @@
 dofile "utils.lua"
 
-local memory = require "memory"
-local system = require "coutil.system"
+local memory<const> = require "memory"
+local system<const> = require "coutil.system"
 
-local argc = select("#", ...)-2
-local format = szfmt..string.rep("z", argc)
-local buffer = memory.create(szlen+maxlen)
+local argc<const> = select("#", ...)-2
+local format<const> = szfmt..string.rep("z", argc)
+local buffer<const> = memory.create(szlen+maxlen)
 local _, index = assert(memory.pack(buffer, format, szlen+1, argc-1, select(3, ...)))
 assert(memory.pack(buffer, szfmt, 1, index-szlen-1))
 
 spawn(function ()
 	local address = assert(system.findaddr(host, port, "s6"))()
-	local conn = assert(system.socket("stream", address.type))
+	local conn<close> = assert(system.socket("stream", address.type))
 	assert(conn:connect(address))
 	assert(conn:send(buffer, 1, index-1))
 	local bytes = 0
@@ -23,7 +23,6 @@ spawn(function ()
 	while bytes < szlen+size do
 		bytes = bytes+assert(conn:receive(buffer, bytes+1))
 	end
-	conn:close()
 	print(">", memory.tostring(buffer, szlen+1, bytes))
 end)
 
