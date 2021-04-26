bbfdbd55e1ab1f41e9838cca7a3ce6cbf7ae2d1a
Renato Maia <maia.renato@gmail.com>
2021-04-25 22:35:55 -0300

    Reorganization of manual sections.

--- doc/maketoc.lua
+++ doc/maketoc.lua
@@ -13,62 +13,84 @@ local function underlined(kind, line, previous)
 	end
 end
 
-local current, previous, sections, section = {}
+local level = 0
+local sections = { [level] = { entries = {} } }
+local previous
+
 local lineno, links = 0, {}
+local contents
+
 for line in file:lines() do
 	lineno = lineno+1
-	if line == "^#%s*Contents%s*$"
-	or underlined("=", line, previous) == "Contents" then
-		sections = {}
-	elseif sections then
-			local name = line:match("^%s*##%s(.-)%s*$")
-		if name then
+	if line == "---" then
+		contents = true
+	elseif contents then
+		local newlevel, name = line:match("^%s*(#+)%s(.-)%s*$")
+		if newlevel then
+			newlevel = #newlevel
 			previous = nil
 		else
-			name = underlined("%-", line, previous)
+			name = underlined("%=", line, previous)
 			if name then
-				table.remove(current, #current)
+				newlevel = 1
+				table.remove(sections[level], #sections[level])
+			else
+				name = underlined("%-", line, previous)
+				if name then
+					newlevel = 2
+					table.remove(sections[level], #sections[level])
+				end
 			end
 		end
-		if name then
-			section = {
+		if name == "Index" and newlevel == 1 then
+			break
+		elseif name then
+			local newsection = {
 				title = name,
 				anchor = makeanchor(name),
 				empty = true,
+				hidden = newlevel > 2,
 				entries = {},
 			}
-			if previous then table.insert(section, previous) end
-			table.insert(section, line)
-			table.insert(sections, section)
-			current = section
+			if previous then table.insert(newsection, previous) end
+			table.insert(newsection, line)
+			local funcname, arguments = name:match("`([%w%.:]+)([^`]-)`")
+			if funcname then
+				newsection.indexed = "`"..funcname.."`"
+				if funcname:find(":", 1, true) then
+					newlevel = newlevel+1
+				end
+			end
+			if newlevel > level then
+				for i = level+1, newlevel-1 do
+					sections[i] = sections[level]
+				end
+				level = newlevel
+			elseif newlevel < level then
+				for i = newlevel+1, level do
+					sections[i] = nil
+				end
+				level = newlevel
+			end
+			table.insert(sections[level-1].entries, newsection)
+			sections[level] = newsection
 		else
-			local name, url = line:match("%[([^%]]+)%]%(([^%)]+)%)")
-			if name then
+			for name, url in line:gmatch("%[([^%]]+)%]%(([^%)]+)%)") do
 				local anchor = url:match("^#([%w%-]+)$")
 				if anchor then
 					table.insert(links, { anchor = anchor, line = lineno })
 				end
 			end
-			if section then
-				if section.empty then
-					section.empty = not line:match("%S")
-					local module = line:match("^Module `([%w%.:]+)`")
-					if module then
-						section.module = "`"..module.."`"
-					end
+			if sections[level].empty then
+				local module = line:match("^Module `([%w%.:]+)`")
+				if module then
+					sections[level].indexed = "`"..module.."`"
+					sections[level].empty = false
 				else
-					local name, extra = line:match("### `([%w%.:]+)([^`]-)`")
-					if name then
-						local entry = {
-							title = "`"..name.."`",
-							anchor = makeanchor(name..extra),
-						}
-						table.insert(section.entries, entry)
-						current = entry
-					end
+					sections[level].empty = not line:match("%S")
 				end
 			end
-			table.insert(current, line)
+			table.insert(sections[level], line)
 		end
 	end
 	previous = line
@@ -76,7 +98,7 @@ end
 
 file:close()
 
-local function writeanchor(prefix, text, entry)
+local function writeanchor(text, entry, prefix)
 	local previous = links[entry.anchor]
 	if previous and previous ~= entry then
 		io.stderr:write("WARN : ", entry.title, " has the same anchor of ", previous.title, "\n")
@@ -88,8 +110,29 @@ local function writeanchor(prefix, text, entry)
 	end
 end
 
+local function writesummary(section, indexed, prefix)
+	if section.indexed then
+		table.insert(indexed, section)
+		indexed[section] = {}
+		indexed = indexed[section]
+	end
+	for i, subsection in ipairs(section.entries) do
+		writeanchor(subsection.title, subsection, not subsection.hidden and prefix.."- " or nil)
+		writesummary(subsection, indexed, prefix.."\t")
+	end
+end
+
+local function writesection(section)
+	for _, line in ipairs(section) do
+		io.write(line, "\n")
+	end
+	for _, subsection in ipairs(section.entries) do
+		writesection(subsection)
+	end
+end
+
 local function sortentry(a, b)
-	return (a.module or a.title) < (b.module or b.title)
+	return (a.indexed or a.title) < (b.indexed or b.title)
 end
 
 local function isorted(t)
@@ -101,52 +144,37 @@ local function isorted(t)
 	return ipairs(sorted)
 end
 
-io.write[[
+local function writeindex(indexed, prefix)
+	for _, section in isorted(indexed) do
+		writeanchor(section.indexed, section, prefix.."- ")
+		writeindex(indexed[section], "\t"..prefix)
+	end
+end
 
+io.write[[
 Summary
 =======
 
 ]]
-for index, section in ipairs(sections) do
-	writeanchor(index..". ", section.title, section)
-end
+local indexed = {}
+writesummary(sections[0], indexed, "")
 
 io.write[[
+- [Index](#index)
 
-Index
-=====
-
+---
 ]]
-for _, section in isorted(sections) do
-	if section.module then
-		writeanchor("- ", section.module, section)
-		for _, entry in isorted(section.entries) do
-			writeanchor("\t- ", entry.title, entry)
-		end
-	else
-		writeanchor(nil, section.title, section)
-	end
-end
-
-io.write[[
-
-Contents
-========
-
-]]
-for _, section in ipairs(sections) do
-	for _, line in ipairs(section) do
-		io.write(line, "\n")
-	end
-	for _, entry in ipairs(section.entries) do
-		for _, line in ipairs(entry) do
-			io.write(line, "\n")
-		end
-	end
-end
+writesection(sections[0])
 
 for _, link in ipairs(links) do
 	if links[link.anchor] == nil then
 		io.stderr:write("ERROR: invalid anchor ", link.anchor, " on line ", link.line, "\n")
 	end
 end
+
+io.write[[
+Index
+=====
+
+]]
+writeindex(indexed, "")
